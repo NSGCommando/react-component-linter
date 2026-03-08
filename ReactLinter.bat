64e4605ca1f5a2f9de8b856f82b5cc5a1583ebd6
NSGCommando <41550763+NSGCommando@users.noreply.github.com>
2026-03-08 19:57:22 +0530

    Ignored 'sigs_public_keys' folder and fixed indent in batch script

--- ReactLinter.bat
+++ ReactLinter.bat
@@ -3,7 +3,7 @@
 :: d   -> Extracts the Drive letter
 :: p   -> Extracts the Folder Path
 :: 0   -> Represents the script itself
-:: %* -> Passes all arguments (files, flags) to the exe
+:: %*  -> Passes all arguments (files, flags) to the exe
 
 :: This line launches the EXE located in the same folder as this script
 :: The empty "" is a quirk of the 'start' command for the window title
